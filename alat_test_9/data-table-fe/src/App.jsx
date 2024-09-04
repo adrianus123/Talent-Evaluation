18f6c5f60cedfb84a6e5b4613caccd6b21296b51
adrianus123 <adrianussimarmata9@gmail.com>
2024-09-04 09:52:57 +0700

    create react project

---
+++ alat_test_9/data-table-fe/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useMemo, useState } from "react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import { AgGridReact } from "ag-grid-react";
+
+const App = () => {
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      filter: true,
+    };
+  });
+
+  return (
+    <div className="ag-theme-quartz" style={{ height: 500 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+      />
+    </div>
+  );
+};
+
+export default App;
