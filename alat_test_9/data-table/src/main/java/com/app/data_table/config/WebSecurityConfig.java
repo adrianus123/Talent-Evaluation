3d3ce59622a9a6b6b1137121fabb22a190b992fc
adrianus123 <adrianussimarmata9@gmail.com>
2024-09-04 09:19:40 +0700

    add cors config

---
+++ alat_test_9/data-table/src/main/java/com/app/data_table/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.app.data_table.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry cors) {
+        cors.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
